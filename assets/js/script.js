f64aefd735d34243b94882792f909aad02e562d3
Avinash Dengani <avinashdengani123@gmail.com>
2021-12-19 02:26:41 +0530

    Done with responsive navbar  and hamburgar working also added cdn for animate.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,39 @@
-const themeSwitch = document.getElementById('switch-color-mode-checkbox-input');
-
-function setColorMode() {
-    if(themeSwitch.checked) {
-        document.documentElement.style.cssText = '--bg-primary: #212429; --color-primary: #ffffff;';
-        $(".logo-img").attr("src", "assets/images/logo/light.png");
-    } else {
-        document.documentElement.style.cssText = '--bg-primary: #ecf0f3; --color-primary: #000000;';
-        $(".logo-img").attr("src", "assets/images/logo/dark.png");
+$(document).ready(function() {
+    
+    //function to set Color Mode
+    function setColorMode(theme) {
+        if(theme.checked) {
+            document.documentElement.style.cssText = '--bg-primary: #212429; --color-primary: #ffffff;';
+            $(".logo-img").attr("src", "assets/images/logo/light.png");
+            for(let i=0; i<themeSwitch.length; i++) {
+                themeSwitch[i].checked = true;
+            }
+        } else {
+            document.documentElement.style.cssText = '--bg-primary: #ecf0f3; --color-primary: #000000;';
+            $(".logo-img").attr("src", "assets/images/logo/dark.png");
+            for(let i=0; i<themeSwitch.length; i++) {
+                themeSwitch[i].checked = false;
+            }
+        }
     }
-}
 
-themeSwitch.addEventListener('change', () => {
-    setColorMode();
+    //Get all inputs to switch darkMode
+    const themeSwitch = $('.switch-color-mode-checkbox-input');
+    
+    // Adding events to all darkmode Inputs
+    for(let i=0; i<themeSwitch.length; i++) {
+        themeSwitch[i].addEventListener('change', () => {
+            setColorMode(themeSwitch[i]);
+        });
+        setColorMode(themeSwitch[i]);
+    }
 });
+
+//HAMBURGAR
+$(document).ready(function(){
+	$('.media-right-nav-hamburgar').click(function(){
+		$(this).toggleClass('open');
+        $(".media-right-nav-list").toggleClass('d-none');
+   });
+});
+
